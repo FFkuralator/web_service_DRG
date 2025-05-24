3572bd47203a484b4bd319028b3dcab671e9c042
FFkuralator <95739275+FFkuralator@users.noreply.github.com>
2025-05-25 03:08:30 +1000

    user.py refactor

--- backend/database/models/user.py
+++ backend/database/models/user.py
@@ -10,12 +10,14 @@ class User:
 
     def create(self, emeil: str, password: str, full_name: str):
         hashed_password = pwd_context.hash(password)
+        
         try:
             self.db.exevute(
                 "INSERT INTO users (email, password_hash, full_name) VALUES (?, ?, ?)",
                 (email, password_hash, full_name)
             )
             return True
+            
         except sqlite3.IntegrityError:
             raise ValueError('Пользователь с таким email уже существует')
     
@@ -25,6 +27,7 @@ class User:
             (email),
             fetch_one=True
         )
+        
         if user and pwd_context.verify(password, user[2]):
             return user[0]
         return None
