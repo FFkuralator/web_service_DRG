6e64712e0ea3df76e476b61ac23ff627d2107d1e
klontaler <fcknguglyandsotired@gmail.com>
2025-06-01 01:49:52 +1000

    routing added

--- app/main.py
+++ app/main.py
@@ -3,6 +3,58 @@ import os
 
 from backend.routes.register import auth_bp
 
+all_spaces = [
+    {
+        "id": 1,
+        "name": "Танцы",
+        "spaces": [
+            {
+                "name": "Пространство",
+                "building": "A",
+                "level": 6,
+                "location": "A666",
+                "description": "Lorem ipsum dolores sit ame",
+                "image_src": "",
+                "image_alt": "",
+            },
+            {
+                "name": "Пространство",
+                "building": "A",
+                "level": 6,
+                "location": "A666",
+                "description": "Lorem ipsum dolores sit ame",
+                "image_src": "",
+                "image_alt": "",
+            }
+
+        ]
+    },
+    {
+        "id": 1,
+        "name": "Не танцы",
+        "spaces": [
+            {
+                "name": "Пространство",
+                "building": "A",
+                "level": 6,
+                "location": "A666",
+                "description": "Lorem ipsum dolores sit ame",
+                "image_src": "",
+                "image_alt": "",
+            },
+            {
+                "name": "Пространство",
+                "building": "A",
+                "level": 6,
+                "location": "A666",
+                "description": "Lorem ipsum dolores sit ame",
+                "image_src": "",
+                "image_alt": "",
+            }
+
+        ]
+    },
+]
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'dev_fallback_key')
@@ -15,6 +67,22 @@ app.register_blueprint(auth_bp)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/space')
+def space():
+    return render_template('spaces/space_card.html')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('spaces/catalog.html', space_categories=all_spaces, category=all_spaces[0])
+
+@app.route('/auth')
+def auth():
+    return render_template('auth/auth.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('auth/profile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
