d324556ae696d8b34176a3c1e3aaaac4eaf148b2
klontaler <fcknguglyandsotired@gmail.com>
2025-06-05 02:04:49 +1000

    index responsive styling

---
+++ app/static/js/base.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    let isOpen = false;
+
+    document.getElementById('burger_button').addEventListener('click', function () {
+        const ani1 = document.getElementById('ani1');
+        const ani1Back = document.getElementById('ani1-back');
+        const ani2 = document.getElementById('ani2');
+        const ani2Back = document.getElementById('ani2-back');
+        const mobileMenu = document.querySelector('.header_nav');
+
+        if (!isOpen) {
+            ani1.beginElement();
+            ani2.beginElement();
+            mobileMenu.classList.toggle('active');
+            document.body.classList.toggle('no-scroll');
+        } else {
+            ani1Back.beginElement();
+            ani2Back.beginElement();
+            mobileMenu.classList.toggle('active');
+            document.body.classList.toggle('no-scroll');
+        }
+
+        isOpen = !isOpen;
+    });
+}
