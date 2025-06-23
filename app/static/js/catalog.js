2ae76bb1c457f587453a8cf20365440f4c64f270
klontaler <fcknguglyandsotired@gmail.com>
2025-06-24 08:28:35 +1000

    Update catalog.js

--- app/static/js/catalog.js
+++ app/static/js/catalog.js
@@ -111,9 +111,6 @@ document.addEventListener('DOMContentLoaded', function() {
             spaces.forEach(space => {
                 const space_item = createElement('li', 'category_item', {},
                     createElement('a', 'space_container', {href: `/space/${space.id}`},
-                        createElement('button', 'fav_button_box', {dataset: {spaceId: space.id}, disabled: true},
-                            createElement('div', `fav_button ${space.is_favorite? active : ''}`, {innerHTML: `<svg width="14" height="14" viewBox="0 0 24 24" fill="{% if space.is_favorite %}red{% else %}currentColor{% endif %}" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>`})
-                        ),
                         space.images[0] ? createElement('img', 'space_image', {src: `/static/assets/${space.images[0].url}`, alt: space.images[0].alt}) : createElement('img', 'space_image', {src: '/static/assets/default_placeholder.jpeg', alt:'No image available'}),
                         createElement('div', 'space_text_container', {},
                             createElement('h3', 'space_name', {textContent: space.name}),
